0710b168bc109279860dbbd2e86fe1690aad5b92
Prishu-04 <pratyakshsrivastava23ks@gmail.com>
2026-02-21 10:18:35 +0530

    Day3 DSA questions

---
+++ Array/easy/bestbuyandsell.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int maxPrice = 0;
+        int maxProfit = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (prices[i] > maxPrice) {
+                maxPrice = prices[i];
+            } else {
+                maxProfit = max(maxProfit, maxPrice - prices[i]);
+            }
+        }
+
+        return maxProfit;
+    }
+};
