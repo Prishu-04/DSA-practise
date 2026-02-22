2e921ff1f01001c4eae57a5559eb93a1925638a2
Prishu-04 <pratyakshsrivastava23ks@gmail.com>
2026-02-22 10:55:16 +0530

    Day4 1 DSA problem

---
+++ Day4/majelem.c
@@ -0,0 +1,14 @@
+int majorityElement(int* nums, int numsSize) {
+    int candidate=0,count=0;
+    for(int i=0;i<numsSize;i++){
+        if(count==0){
+            candidate=nums[i];
+        }
+        if(nums[i]==candidate){
+            count++;
+        }else{
+            count--;
+        }
+    }
+    return candidate;
+}
